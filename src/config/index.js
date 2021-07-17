b87927cfc8f8fc9d53ccd10c32e64a09a5d4b051
Michael Serajnik <m@mser.at>
2021-07-17 18:56:23 +0200

    Release 1.3.1

--- src/config/index.js
+++ src/config/index.js
@@ -13,7 +13,7 @@ if (frozenEmotesPath.startsWith('.')) {
 }
 
 module.exports = {
-  version: '1.3.0',
+  version: '1.3.1',
   apiVersion: 2,
   publicUrl: process.env.EMOTE_SERVER_PUBLIC_URL || 'http://localhost',
   port: process.env.EMOTE_SERVER_PORT || 8000,
